9c6e2b94100dc920ebded6b70df2e4e89a18b932
FakeGods <w.leszkowicz99@gmail.com>
2024-02-23 22:41:28 +0100

    123

---
+++ bar/src/components/Contact/Contact.jsx
@@ -0,0 +1,18 @@
+function Contact () {
+ return (
+    <>
+        <section id="contact" className="contact">
+            <div className="container">
+                <div className="contact_content">
+                    <div className="contact_title">
+                        <button id="contact-btn">CONTACT</button>
+                        <div className="contact-form" ></div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </>
+ )
+}
+
+export default Contact;
---
+++ bar/src/components/Projects/Projects.jsx
@@ -0,0 +1,21 @@
+
+
+
+function Projects() {
+    return(
+        <section id="projects" className="project">
+            <div className="container">
+                <div className="project_content">
+                    <p>videos</p>
+                    <h3>music here</h3>
+                    <div className="video"></div>
+                    <p>photos</p>
+                    <h3>photos here</h3>
+                    <div className="photos"></div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Projects;
