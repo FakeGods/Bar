29ee84b63397aa51dbf8d0cca67fcc429e7ce0f3
FakeGods <w.leszkowicz99@gmail.com>
2024-03-10 12:52:18 +0100

    All imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import "./App.css";
+import Nav from "./components/NavBar/Nav";
+import Hero from "./components/Hero/Hero";
+import Footer from "./components/Footer/Footer";
+import Contact from "./components/Contact/Contact";
+import About from "./components/About/About";
 
 export default function App() {
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
