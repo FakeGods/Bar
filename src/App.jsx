dbac452f161b747cb7905282a21f0b9c35eef6d0
FakeGods <w.leszkowicz99@gmail.com>
2024-03-11 16:22:15 +0100

    ??

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./App.css";
-import Navbar from "./components/NavBar/Navbar";
+import Nav from "./components/NavBar/Nav";
 import Hero from "./components/Hero/Hero";
 import Footer from "./components/Footer/Footer";
 import Contact from "./components/Contact/Contact";
 import About from "./components/About/About";
 
 export default function App() {
-  return <Navbar />;
+  return <Nav />;
 }
