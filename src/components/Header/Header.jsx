c05e1f48731bca2c5d601665370bb2c19275c532
FakeGods <w.leszkowicz99@gmail.com>
2024-03-13 17:05:27 +0100

    logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import logo from "../../assets/logo.png";
+import { logo } from "../../assets/logo.png";
 
 const Header = () => {
   return (
     <>
       <div className="fixed top-0 z-50 bg-n-8/90 backdrop-blur-sm border-b">
         <div className="flex items-center px-5 lg:px-7.5 xl:px-10 max-lg:py-4">
-          <a className="block w-[12rem] xl:mr-8" href="#">
+          <a className="block w-[12rem] xl:mr-8" href="#hero">
             <img src={logo} width={100} height={40} alt="logo" />
           </a>
         </div>
